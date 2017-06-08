5eec2944e5891baccd56bfb009633ac40960d135
Quentin <Quentin_L_0415@outlook.com>
2017-06-08 23:10:37 +0800

    Fix some bugs

--- uncompress.rb
+++ uncompress.rb
@@ -1,11 +1,17 @@
+# current problem: CL does not accept file names with braces 
+# try to add "\" before braces 
+
+
+
 $regex = /.*\/(.*)\.rar/
 
-def input
+def input()
     case ARGV[0] 
     when "-p" then 
         password = ARGV[1]
         listOfFiles = convertAddress(2)
-        processFiles listOfFiles 
+        puts password 
+        processFiles(listOfFiles, password)
     else 
         listOfFiles = convertAddress(0)
         processFiles listOfFiles
@@ -16,35 +22,60 @@ def convertAddress(startIndex)
     startIndex.times do 
         ARGV.shift 
     end 
+
+    result = [] 
     
     ARGV.each do |path| 
         case path[0]
         when '/' then 
-            return [address]
+            result.push path
         when '.' then 
             arr = Dir.entries(".")
             arr.shift 
-            return arr.shift 
+            arr.shift 
+            puts arr
+            result + arr 
         else 
-            pwd = File.basename(Dir.getwd)
-            return [pwd + address]
+            pwd = Dir.getwd + '/'
+            result.push(pwd + path)
         end 
     end 
+    return result 
 end 
 
 # process the list of compressed file paths 
 def processFiles(list) 
    list.each do  |path|
       name = getFileName path
+      puts name 
       system ("mkdir #{name}")
       system ("cd #{name}")
+      system ("unrar ../#{name}.rar")
+      #system ("rm ../#{name}.rar")
    end 
+end 
+
+def processFiles(list, password) 
+    list.each do |path| 
+        name = getFileName path 
+        puts name 
+        system ("mkdir #{name}")
+        system ("cd #{name}")
+        system ("unrar e #{path}")
+        #system ("rm ../#{name}.rar")
+        system (password) 
+    end 
 end 
 
 # use regular expression to extract the 
 # file name from the path 
 def getFileName(path) 
-    return $regex.match(path)[1]
+    return removeBraces($regex.match(path)[1])
 end 
 
-puts(getFileName("/hello.rar"))
+def removeBraces(s) 
+    return s.delete("()").gsub(/\s+/, "")
+end 
+
+def format(s) 
+end 
